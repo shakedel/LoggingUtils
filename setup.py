4aaf939070e81ce165037b568a9abb414648702d
Max Paradis <max@faeriol.me>
2017-08-22 22:08:30 -0400

    Clean setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,10 @@ from distutils.core import setup
 setup(
     name='LoggingUtils',
     packages=['LoggingUtils'],
-    version='1.0.0',
+    version='1.0.1',
     description='Additional utilities for Python logging.',
     author='Max Paradis',
     author_email='max@faeriol.me',
     url='https://github.com/faeriol/LoggingUtils',
-    keywords=['logging', 'utilities'],
-    install_requires=[],
-    classifiers=[]
+    keywords=['logging', 'utilities']
 )
