c036a45e6788e76b2b38376c84bdd84889bfb3ad
Max Paradis <max@faeriol.me>
2018-01-27 23:56:55 -0500

    Fixup packaging details

--- setup.py
+++ setup.py
@@ -5,12 +5,19 @@ Setup script for LoggingUtils
 from distutils.core import setup
 
 setup(
-    name='LoggingUtils',
-    packages=['LoggingUtils'],
-    version='1.0.1',
+    name='logging-utils',
+    packages=['loggingutils'],
+    version='1.0.2',
     description='Additional utilities for Python logging.',
     author='Max Paradis',
     author_email='max@faeriol.me',
     url='https://github.com/faeriol/LoggingUtils',
-    keywords=['logging', 'utilities']
+    keywords=['logging', 'utilities'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ]
 )
